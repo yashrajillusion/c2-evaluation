5e951f06046581187f031582cac0fb1a33214326
yashrajillusion <95868808+yashrajillusion@users.noreply.github.com>
2022-03-14 10:55:03 +0530

    deployment

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Rentals } from "./Components/Rentals/Rentals";
+import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState,useRef } from "react";
+
+function App() {
+  const [toggle, setToggle] = useState(true)
+  const ref = useRef(true)
+  
+  const toggleHandle = (re,toggle)=>{
+    if(ref.current){
+      ref.current = false;
+      setToggle(p=>!p);
+    }else{
+      ref.current = false;
+      setToggle(p=>!p);
+    }
+  }
+  
+  return (
+    <div className="App">
+      <button onClick={toggleHandle} className="toggleForm">
+        { toggle?"Add House":"Show Rentals"/* Show text Add House or Show Rentals based on state */}
+      </button>
+      {toggle?<Rentals/>:<AddHouse/>}
+      <br />
+    </div>
+  );
+}
+
+export default App;
