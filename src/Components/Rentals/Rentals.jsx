d999fdeece021ba36e0dd005f99b5fb45fc0bc5f
yashrajillusion <95868808+yashrajillusion@users.noreply.github.com>
2022-03-24 17:50:41 +0530

    search bar

--- src/Components/Rentals/Rentals.jsx
+++ src/Components/Rentals/Rentals.jsx
@@ -1,42 +1,91 @@
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./Rentals.css";
 import axios from "axios";
 export const Rentals = () => {
-  const [db,setDb] = useState([])
+  const [db, setDb] = useState([]);
 
-
-  useEffect(()=>{
-    let data = []
-    axios.get("http://localhost:8080/houses").then((res)=>{
-      return res
-    }).then((res)=>{
-      data = res.data
-    })
-    try{
-      axios.get("https://giftcartbackendapp.herokuapp.com/api/products").then((res)=>{
-      
-      data.map((el,i)=>el.image=res.data[i].image)
-      setDb(data)
-    })
-
-    }catch(err){
-      console.log(err)
+  useEffect(() => {
+    let data = [];
+    axios
+      .get("http://localhost:8080/houses")
+      .then((res) => {
+        return res;
+      })
+      .then((res) => {
+        data = res.data;
+      });
+    try {
+      axios
+        .get("https://giftcartbackendapp.herokuapp.com/api/products")
+        .then((res) => {
+          data.map((el, i) => (el.image = res.data[i].image));
+          setDb(data);
+        });
+    } catch (err) {
+      console.log(err);
     }
-    
-    
-  },[])
+  }, []);
 
-  
   return (
     <div className="rentalContainer">
       <div className="sortingButtons">
-        <button onClick={()=>{ let arr = db.sort((a,b)=>b.id-a.id); setDb([...arr]) }} className="sortById">Sort by ID</button>
-        <button onClick={()=>{let arr = db.sort((a,b)=>+a.rent- +b.rent); setDb([...arr])}} className="sortByRentAsc">Rent Low to high</button>
-        <button onClick={()=>{let arr = db.sort((a,b)=> +b.rent - +a.rent); setDb([...arr])}} className="sortByRentDesc">Rent High to low</button>
-        <button onClick={()=>{let arr = db.sort((a,b)=> +a.areaCode - +b.areaCode); setDb([...arr])}} className="sortByAreaAsc">Area Low to high</button>
-        <button onClick={()=>{let arr = db.sort((a,b)=> +b.areaCode - +a.areaCode); setDb([...arr])}} className="sortByAreaDesc">Area High to Low</button>
+        <button
+          onClick={() => {
+            let arr = db.sort((a, b) => b.id - a.id);
+            setDb([...arr]);
+          }}
+          className="sortById"
+        >
+          Sort by ID
+        </button>
+        <button
+          onClick={() => {
+            let arr = db.sort((a, b) => +a.rent - +b.rent);
+            setDb([...arr]);
+          }}
+          className="sortByRentAsc"
+        >
+          Rent Low to high
+        </button>
+        <button
+          onClick={() => {
+            let arr = db.sort((a, b) => +b.rent - +a.rent);
+            setDb([...arr]);
+          }}
+          className="sortByRentDesc"
+        >
+          Rent High to low
+        </button>
+        <button
+          onClick={() => {
+            let arr = db.sort((a, b) => +a.areaCode - +b.areaCode);
+            setDb([...arr]);
+          }}
+          className="sortByAreaAsc"
+        >
+          Area Low to high
+        </button>
+        <button
+          onClick={() => {
+            let arr = db.sort((a, b) => +b.areaCode - +a.areaCode);
+            setDb([...arr]);
+          }}
+          className="sortByAreaDesc"
+        >
+          Area High to Low
+        </button>
       </div>
-      <input onChange={({target})=>{ let arr = db.filter((el)=>{ if(el.address===target.value){return el}}); if(arr.length!=0){setDb([...arr])} }}
+      <input
+        onChange={({ target }) => {
+          let arr = db.filter((el) => {
+            if (el.address.startsWith(target.value)) {
+              return el;
+            }
+          });
+          if (arr.length != 0) {
+            setDb([...arr]);
+          }
+        }}
         className="searchAddress"
         type="text"
         placeholder="Search Address"
@@ -65,10 +114,16 @@ export const Rentals = () => {
                 <td className="areaCode">{house.areaCode}</td>
                 <td className="rent">{house.rent}</td>
                 <td className="preferredTenants">
-                  {house.preferredTenant/* Show text Both or Bachelors or Married based on values */}
+                  {
+                    house.preferredTenant /* Show text Both or Bachelors or Married based on values */
+                  }
                 </td>
                 <td className="houseImage">
-                  <img style={{"width":"100px","height":"100px"}} src={house.image} alt="house" />
+                  <img
+                    style={{ width: "100px", height: "100px" }}
+                    src={house.image}
+                    alt="house"
+                  />
                 </td>
               </tr>
             );
